97861ba1d7707438c2e4cca809e50fd107a3e2b6
yamashun99 <yamashun.103099@gmail.com>
2023-08-26 19:12:19 +0900

    add cuttaux

--- conf.py
+++ conf.py
@@ -6,7 +6,6 @@ import copy
 import random
 import collections
 
-
 def gen_uniformly_events(beta, lam):
     n = 0
     d = np.exp(-beta*lam)
@@ -49,24 +48,47 @@ class Cuttau():
         s += '--\n'
         return s
 
+    def __str__(self,):
+        s = ''
+        for itau in range(len(self.cuttau)):
+            s += str(self.cuttau[itau].t) + ' ' + \
+                str(self.cuttau[itau].s) + '\n'
+        return s
+    
+    def _getitau(self, tau):
+        return bisect.bisect_left(self.cuttau, tau, key=lambda x: x.t) - 1
+
     def remove_cut(self,):
         removed_cut = []
         for itau in range(len(self.cuttau)):
-            print(itau)
             if self.cuttau[itau].s*self.cuttau[itau-1].s < 0:
                 removed_cut.append(self.cuttau[itau])
         if len(removed_cut) == 0:
             removed_cut.append(
-                self.cut(0, self.cut[0].s, True))
-        self.cut = removed_cut
+                self.cut(0, self.cuttau[0].s, True))
+        self.cuttau = removed_cut
 
     def add_cut(self,):
-        for ix in range(self.L):
-            tauk, n = gen_uniformly_events(self.beta, self.Hx/2.0)
-            if n > 0:
-                confitaus = [bisect.bisect_left()]
-                self.cut[ix].append(
-                    self.cuttau(ix, tauk[itau], random.choice([-1, 1]), False))
-                cutt = [self.cuttau(ix, tauk[itau], random.choice([-1, 1]), False)
-                        for itau in range(n)]
-            self.cut[ix] += cutt
+        tauk, n = gen_uniformly_events(self.beta, self.Hx/2.0)
+        for tau in tauk:
+            itau = self._getitau(tau)
+            spin = self.cuttau[itau].s
+            bisect.insort(self.cuttau, self.cut(tau, spin, False), key=lambda x: x.t)
+        if self.cuttau[0].null == True and len(self.cuttau) > 1:
+            self.cuttau.pop(0)
+
+class Cuttaux():
+    def __init__(self, beta, Hx, L):
+        self.beta = beta
+        self.Hx = Hx
+        self.L = L
+        self.cuttaux = [Cuttau(beta, Hx) for i in range(L)]
+
+    def __getitem__(self, position):
+        return self.cuttaux[position]
+
+    def __repr__(self,):
+        s = ''
+        for ct in self.cuttaux:
+            s += str(ct) + '--\n'
+        return s
